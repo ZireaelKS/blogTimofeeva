4d1a0db6cdb0936d37dcaea1c3a24da1a87c567a
ZireaelKS <Iliveyou2610@gmail.ru>
2020-09-17 22:29:47 +0400

    Добавьте файлы проекта.

---
+++ blogTimofeeva/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blogTimofeeva.Controllers
+{
+    /// <summary>
+    ///  контроллер для работы с блогом
+    /// </summary>
+    public class BlogController : Controller
+    {
+        /// <summary>
+        /// страница блога
+        /// </summary>
+        /// <returns>код htmlcs блога</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ blogTimofeeva/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using blogTimofeeva.Models;
+
+namespace blogTimofeeva.Controllers
+{
+    /// <summary>
+    /// контроллер для работы с главной страницей
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// переменная для ведения журнала
+        /// </summary>
+        private readonly ILogger<HomeController> _logger;
+
+        /// <summary>
+        /// Конструктор класса HomeController
+        /// </summary>
+        /// <param name="logger"></param>
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// главная страница
+        /// </summary>
+        /// <returns>код htmlcs главной страницы</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// страница конфиденциальности
+        /// </summary>
+        /// <returns>код htmlcs страницы конфиденциальности</returns>
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// страница ошибка
+        /// </summary>
+        /// <returns>код htmlcs с ошибкой</returns>
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
